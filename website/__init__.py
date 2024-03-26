151bc7bd63cbb07daee94be8f3a298eb88b7e6fe
GabrielMonginho <gabriel.monginho2@gmail.com>
2024-03-26 04:25:14 +0000

    3.5

--- website/__init__.py
+++ website/__init__.py
@@ -18,8 +18,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 if os.getenv("DATABASE_USER") and os.getenv("DATABASE_PASSWORD"):
     database_user = os.getenv('DATABASE_USER')
     database_password = os.getenv('DATABASE_PASSWORD')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{database_user}:{database_password}@\\database?unix_socket=\\cloudsql\\luxurywheels:europe-southwest1:database"
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{database_user}:{database_password}@/database?unix_socket=/cloudsql/luxurywheels:europe-southwest1:database"
+
 elif os.getenv("DATABASE_URL"):
     # Use DATABASE_URL if defined
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
